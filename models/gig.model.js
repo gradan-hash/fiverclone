3b6fecf398d83045f1590e924778e7b36d7494ba
gradan-hash <mutukunyaa@gmail.com>
2023-09-12 23:14:29 +0300

    models

--- models/gig.model.js
+++ models/gig.model.js
@@ -3,8 +3,68 @@ const { Schema } = mongoose;
 
 const gigScheme = new Schema(
   {
-    
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    totalStars: {
+      type: Number,
+      default: 0,
+    },
+    starNumber: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: false,
+    },
+    shorttitle: {
+      type: String,
+      required: true,
+    },
+    shortdesc: {
+      type: String,
+      required: true,
+    },
+    deliverytime: {
+      type: Number,
+      required: true,
+    },
+    revisionNumber: {
+      type: Number,
+      required: true,
+    },
+    features: {
+      type: [String],
+      required: false,
+    },
+    sales: {
+      type: Number,
+      required,
+    },
   },
+
   { timestamps: true }
 );
 
