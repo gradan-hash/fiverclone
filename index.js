794292254f15e966c8ddde13aabf376273a6f225
gradan-hash <mutukunyaa@gmail.com>
2023-09-12 22:45:02 +0300

    ...

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("database connection online");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.listen(4000, () => {
+  connect()
   console.log("backend running");
 });
-
-
