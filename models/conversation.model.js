42200103cf451785d9b3f78bee559832429e76b4
gradan-hash <mutukunyaa@gmail.com>
2023-09-13 07:39:45 +0300

    model ddone

---
+++ models/conversation.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const conversationSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    sellerId: {
+      type: String,
+      required: true,
+    },
+    buyerId: {
+      type: String,
+      required: true,
+    },
+    readByseller: {
+      type: Boolean,
+      required: true,
+    },
+    readBybuyer: {
+      type: Boolean,
+      required: true,
+    },
+    lastmessage: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("conversation", conversationSchema);
