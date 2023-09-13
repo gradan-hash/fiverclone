2ba7dc40afe352bcb59b252505582b41f28d1631
gradan-hash <mutukunyaa@gmail.com>
2023-09-13 09:24:21 +0300

    login/reg

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,8 +3,11 @@ import user from "../models/user.model.js";
 import bcrypt from "bcrypt";
 export const register = async (req, res) => {
   try {
-    const hash = bcypt.hashSync(req.body.password);
-    const newUser = new user(req.body);
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new user({
+      ...req.body,
+      password: hash,
+    });
 
     await newUser.save();
     res.status(201).send("success");
@@ -12,5 +15,20 @@ export const register = async (req, res) => {
     res.status(500).send("something went wrong");
   }
 };
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const User = await user.findOne({ username: req.body.username });
+
+    if (!User) return res.status(404).send("user not found");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, User.password);
+    if (!isCorrect) return res.status(400).send("wrong password or username");
+
+    const { password, ...info } = User._doc;
+
+    res.status(200).send(info);
+  } catch (err) {
+    res.status(500).send("something went wrong");
+  }
+};
 export const logout = async (req, res) => {};
