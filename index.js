d2cf20e7d15430c65be7a1524629a17188558beb
gradan-hash <mutukunyaa@gmail.com>
2023-09-15 16:18:52 +0300

    debug

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ import orderRoute from "./routes/order.route.js";
 import conversationRoute from "./routes/conversation.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-
-
+import cors from "cors";
 const app = express();
 dotenv.config();
 mongoose.set("strictQuery", true);
@@ -24,10 +23,9 @@ const connect = async () => {
   }
 };
 
-
-
-app.use(express.json())
-app.use(cookieParser())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
@@ -37,12 +35,11 @@ app.use("/api/review", reviewRoute);
 app.use("/api/order", orderRoute);
 app.use("/api/conversation", conversationRoute);
 
-
-app.use((req,res,next,err)=>{
+app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "something went wrong";
 
-  return res.status(errorStatus).send(errorMessage)
+  return res.status(errorStatus).send(errorMessage);
 });
 
 app.listen(4000, () => {
