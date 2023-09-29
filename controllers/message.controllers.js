3b0bd3d09ac1ea5c56243ad3ca894fa755efbb6c
gradan-hash <mutukunyaa@gmail.com>
2023-09-29 17:22:44 +0300

    wewew

--- controllers/message.controllers.js
+++ controllers/message.controllers.js
@@ -1,14 +1,28 @@
 import express from "express";
 import Message from "../models/message.model.js";
 import createError from "../utils/createError.js";
+import Conversation from "../models/conversation.model.js";
 
-export const createMessage = (req, res, next) => {
+export const createMessage = async (req, res, next) => {
+  const newMessage = new Message({
+    conversationId: req.body.conversationId,
+    userId: req.userId,
+    desc: req.body.desc,
+  });
   try {
-    const newMessage = new Message({
-      conversationId: req.body.conversationId,
-      userId: req.userId,
-      desc: req.body.desc,
-    });
+    const savedMessage = await newMessage.save();
+    await Conversation.findOneAndUpdate(
+      { id: req.body.conversationId },
+      {
+        $set: {
+          readByseller: req.isSeller,
+          readBybuyer: !req.isSeller,
+          lastmessage: req.body.desc,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).send(savedMesssaage);
   } catch (err) {
     next(err);
   }
